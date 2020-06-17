a77024e60c09f7030e874f9ac07c18a1b6135b83
fjch100 <fjch100@gmail.com>
2020-06-17 14:44:35 -0500

    increse token time to 1 day

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // 60 min
 // 24 hr
 // 30 dias
-process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+process.env.CADUCIDAD_TOKEN = '1d';
 
 //===================
 // SEED
